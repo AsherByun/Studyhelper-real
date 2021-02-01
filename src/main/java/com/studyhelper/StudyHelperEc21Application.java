69154f237f5d61347e5a124bebe2931012ca4389
변성재 <dfdo333@naver.com>
2021-02-01 21:07:35 +0900

    init

---
+++ src/main/java/com/studyhelper/StudyHelperEc21Application.java
@@ -0,0 +1,13 @@
+package com.studyhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperEc21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperEc21Application.class, args);
+	}
+
+}
