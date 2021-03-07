d94693a94bf9717357716f6c99bc87d935b95485
변성재 <dfdo333@naver.com>
2021-03-07 21:31:57 +0900

    리팩토링

--- ST-WEB/src/main/java/com/studyhelper/WebApplication.java
+++ ST-WEB/src/main/java/com/studyhelper/WebApplication.java
@@ -20,8 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @EntityScan(basePackages = {"com.studyhelper.domain"})
 @SpringBootApplication
 public class WebApplication {
+	//yml 위치정보 지정
 	public static String APPLICATION_LOCATIONS = "spring.config.location=";
+	//배포환경의 yml 위치
 	public static String LOCAL_LOCATION = "C:/Users/Owner/eclipse-workspace/config/application-real.yml";
+	//local환경의 yml 위치
 	public static String DEPLOY_LOCATION = "/home/ubuntu/app/travis/config/application-real.yml";
 
 	public static void main(String[] args) {
@@ -30,19 +33,14 @@ public class WebApplication {
 	}
 
 	public static void setProperties() {
+		//local환경과 deploy환경의 운영체제가 다르기때문에 따로 path를 지정해주었음
 		File local = new File(LOCAL_LOCATION);
 		File deploy = new File(DEPLOY_LOCATION);
-
 		if (local.exists()) {
 			APPLICATION_LOCATIONS += LOCAL_LOCATION;
 		}
 		if (deploy.exists()) {
 			APPLICATION_LOCATIONS += DEPLOY_LOCATION;
 		}
-	}
-	
-	@GetMapping("web")
-	public String Web() {
-		return "web hello";
 	}
 }
