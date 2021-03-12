7720d35674fc4afce2780edb81684cdfc12b339e
변성재 <dfdo333@naver.com>
2021-03-12 15:43:21 +0900

    deploy.sh -> start.sh 실행대신 복사만

--- src/test/java/com/studyhelper/StudyHelperEc21ApplicationTests.java
+++ src/test/java/com/studyhelper/StudyHelperEc21ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.studyhelper;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
