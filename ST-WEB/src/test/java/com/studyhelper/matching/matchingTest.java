34a9d37cf77efc0d46e9562a03b9a2ef836056a1
변성재 <dfdo333@naver.com>
2021-03-13 15:53:41 +0900

    travis  skip test

--- ST-WEB/src/test/java/com/studyhelper/matching/matchingTest.java
+++ ST-WEB/src/test/java/com/studyhelper/matching/matchingTest.java
@@ -1,6 +1,8 @@
 package com.studyhelper.matching;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import javax.transaction.Transactional;
 
@@ -9,14 +11,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studyhelper.StudyhelperTest;
+import com.studyhelper.domain.matching.entity.Matching;
 import com.studyhelper.domain.matching.entity.enums.Gender;
+import com.studyhelper.domain.matching.entity.enums.Region;
+import com.studyhelper.domain.matching.entity.enums.Subject;
 import com.studyhelper.domain.member.entity.Member;
 import com.studyhelper.domain.member.entity.enums.Role;
 import com.studyhelper.domain.member.repo.MemberRepository;
@@ -67,4 +75,35 @@ public class matchingTest {
 		Member member = memberRepository.findById("dfdo333").get();
 		System.out.println(member.getId());
 	}
+	
+	@Test
+	@Transactional
+	@WithUserDetails(value = "king0", userDetailsServiceBeanName = "memberDetailService")
+	void 매칭테스트() throws Exception {
+		Matching matching = new Matching();
+		matching.setRegion(Region.SEOUL_SHINCHON);
+		matching.setSize(4);
+		matching.setSubject(Subject.PROGRAMMING_C);
+		String content = objectMapper.writeValueAsString(matching);
+		
+		mvc.perform(post("/matching/request").content(content).contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().is3xxRedirection()).andDo(print());
+	}
+//	@Test
+//	@Transactional
+//	void 회원가입() throws Exception {
+//		Member member = new Member();
+//		member.setAge(22);
+//		member.setGender(Gender.MAN);
+//		member.setId("dfdo1995");
+//		member.setName("sungjaebyeun");
+//		member.setNickName("byeun");
+//		member.setPassword("1234");
+//		String content = objectMapper.writeValueAsString(member);
+//		
+//		System.out.println(content);
+//		
+//		mvc.perform(post("/signup").content(content).contentType(MediaType.APPLICATION_JSON)
+//				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print());
+//	}
 }
